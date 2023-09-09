16febe6778c1902bf0b21769244e0f5cad1b262a
Michael Kalango <michaelkalango@gmail.com>
2023-09-09 12:09:52 +0100

    feat: typography basic

---
+++ lib/ui/common/app_text_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyles {
+  static TextStyle headlineBold24 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.normal,
+    fontFamily: 'Satoshi',
+    fontSize: 24.sp,
+  );
+
+  static TextStyle titleRegular16 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.normal,
+    fontFamily: 'Satoshi',
+    fontSize: 16.sp,
+  );
+}
