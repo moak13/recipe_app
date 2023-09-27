b8d726631093ddc0713025cc8df4998ca5447555
DreXuri <balogundamilaree@gmail.com>
2023-09-27 13:33:53 +0100

    add a white backgrond for app bar

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -6,6 +6,7 @@ const Color kcDarkGreyColor = Color(0xFF1A1B1E);
 const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
+const Color kcBackground = Color(0xFFFFFFFF);
 const Color kcBackgroundColor = kcDarkGreyColor;
 
 class AppColors {
